85d26a665595dd3365b9307c509cff7cb283d3b0
tapatsawin <128007324+tapatsawin@users.noreply.github.com>
2026-02-11 07:56:36 +0000

    add 3 file

--- main.c
+++ main.c
@@ -2,30 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Node.h"
-
 #include "Queue.h"
 
 int main(int argc , char **argv) {
-  NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
-/* For struct Queue
-  Queue  q;
-   q. headPtr=NULL;
-   q.tailPtr=NULL;
-   q.size=0;
-*/
-   int i,x;
-   
+  /* For struct Queue */
+  Queue q;
+  q.headPtr=NULL;
+  q.tailPtr=NULL;
+  q.size=0;
 
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
-            printf("dequeing %d\n",x);
-        }
-        else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
-           
-        }
- }
+  int i,x;
+  for(i=1;i<argc;i++){
+    if(strcmp(argv[i],"x")==0){
+      if(q.size > 0){
+        x = dequeue_struct(&q);
+        printf("dequeing %d\n", x);
+      }
+    }
+    else if(strcmp(argv[i],"s")==0){
+      if(q.size > 0){
+        x = dequeue_struct(&q);
+        printf("popping %d\n", x);
+      }
+    }
+    else {
+      enqueue_struct(&q, atoi(argv[i]));
+    }
+  }
+
+  if(q.size > 0){
+    printf("Remaining in queue: ");
+    while(q.size > 0){
+      printf("%d ", dequeue_struct(&q));
+    }
+    printf("\n");
+  }
+
   return 0;
 }
